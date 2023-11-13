875bfd2de97ef9ec32b67dc37ca09a8a5e686e3c
User <User@DESKTOP-U1QL1NB>
2023-11-13 16:07:48 +0300

    Добавьте файлы проекта.

---
+++ WebApplication20/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication20.Models;
+
+namespace WebApplication20.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> contextOptions) 
+            : base(contextOptions)  
+        {
+            
+        }  
+
+        //
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ WebApplication20/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication20.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public int Age { get; set; }
+
+        public int Salary { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+    }
+}
